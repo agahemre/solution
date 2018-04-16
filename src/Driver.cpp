31f0605fb97936d63de32e7a63ba107fe6d3321e
emrecanagah <agah.emrecan@gmail.com>
2018-04-16 18:54:14 +0300

    NEW TEST CASES added for newly added functions (+)

--- src/Driver.cpp
+++ src/Driver.cpp
@@ -34,26 +34,53 @@ vector<int> construct_demo_vector()
 	return v1;
 }
 
+vector<int> construct_demo_vector_custom_sort()
+{
+	vector<int> v1;
+	v1.push_back(3);
+	v1.push_back(1);
+	v1.push_back(2);
+	v1.push_back(2);
+	v1.push_back(4);
+	v1.push_back(6);
+	v1.push_back(10);
+	v1.push_back(9);
+	v1.push_back(8);
+	v1.push_back(3);
+	v1.push_back(6);
+	v1.push_back(2);
+	v1.push_back(4);
+	v1.push_back(13);
+	v1.push_back(9);
+	v1.push_back(7);
+	v1.push_back(6);
+
+	return v1;
+}
+
 vector<const char *> construct_demo_vector_v2()
 {
 	vector<const char *> v1;
-	v1.push_back("Emrecan");
-	v1.push_back("Eren");
-	v1.push_back("Emrecan");
-	v1.push_back("Emma");
-	v1.push_back("Canan");
-	v1.push_back("Canan");
-	v1.push_back("Burcu");
-	v1.push_back("Emrecan");
-	v1.push_back("Cem");
-	v1.push_back("Emrecan");
-	v1.push_back("Eren");
-	v1.push_back("Aysen");
-	v1.push_back("Emrecan");
-	v1.push_back("Cem");
-	v1.push_back("Canan");
-	v1.push_back("Emrecan");
-	v1.push_back("Anil");
+	v1.push_back("canan");
+	v1.push_back("emrecan");
+	v1.push_back("emrecan");
+	v1.push_back("emma");
+	v1.push_back("canan");
+	v1.push_back("canan");
+	v1.push_back("burcu");
+	v1.push_back("canan");
+	v1.push_back("emrecan");
+	v1.push_back("cem");
+	v1.push_back("emrecan");
+	v1.push_back("eren");
+	v1.push_back("aysen");
+	v1.push_back("canan");
+	v1.push_back("canan");
+	v1.push_back("emrecan");
+	v1.push_back("cem");
+	v1.push_back("canan");
+	v1.push_back("emrecan");
+	v1.push_back("anil");
 
 	return v1;
 }
@@ -61,6 +88,7 @@ vector<const char *> construct_demo_vector_v2()
 int main()
 {
 	vector<int> v = construct_demo_vector();
+	vector<int> c_sort = construct_demo_vector_custom_sort();
 	vector<const char *> v2 = construct_demo_vector_v2();
 	int sze = v.size();
 	const int target = 71;
@@ -72,6 +100,12 @@ int main()
 	string s("anilella");
 	string s0("anil");
 	string str("aca");
+	// string a("abc");
+	 string a("stuvwx");
+	// string w("jdzsnpejkyqktrdlljfuozlfsvrhqrwgtojnxllzmbfvoexxxttzcbprwzyztsovcxootxoswbffcokhtosfvn");
+	string b("abc");
+	//string b("def");
+	string w("keyhbmeknckfavrpqyyfjxjjmivtsftikovkcdcwefctqrqjryhtlcvstm");
 
 	// debug purpose
 	printf ("The target index -> %d\n", index);
@@ -92,6 +126,16 @@ int main()
 	printf ("Compare %s and %s, result -> %d\n", s.c_str(), s0.c_str(), solution::compare(s, s0) );
 	printf ("Compare %s and %s, result -> %d\n", s.c_str(), s0.c_str(), s.compare(s0) ); // TEST WITH BUILT-IN FUNCTION
 	printf ("Check given string %s is whether palindrome or not, result -> %d\n", str.c_str(), solution::is_palindrome(str) );
+	printf ("Merge given string %s and %s, result -> %s\n", a.c_str(), b.c_str(), solution::merge_str(a, b).c_str() );
+
+	for (auto const& i : c_sort)
+		printf ("%d ", i);
+	printf ("\nCustom sort performed\n");
+	solution::custom_sort(c_sort);
+
+	for (auto const& j : c_sort)
+		printf ("%d ", j);
+	printf("\n");
 
 	return solution::SUCCESS;
 }
